9d02ed164855e80bcf0222162549b08ca8f32a28
stan e <yi.hang@live.com>
2018-03-29 23:54:28 +0800

    修改

--- src/app/pages/organization-units/organization-unit-members.component.ts
+++ src/app/pages/organization-units/organization-unit-members.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit, Injector } from '@angular/core';
 import { OrganizationUnitServiceProxy, OrganizationUnitUserListDto } from '@shared/service-proxies/service-proxies';
 import { AppComponentBase, PagedListingComponentBase, PagedRequestDto } from '@shared/component-base';
-
+import { IBasicOrganizationUnitInfo } from './basic-organization-unit-info';
 
 @Component({
     selector: 'organization-unit-members',
     templateUrl: './organization-unit-members.component.html'
 })
 export class OrganizationUnitOnMembersComponent extends PagedListingComponentBase<OrganizationUnitUserListDto> {
+
+    private _organizationUnit: IBasicOrganizationUnitInfo = null;
     loading = false;
     dataItems: OrganizationUnitUserListDto[] = [];
 
@@ -21,27 +23,51 @@ export class OrganizationUnitOnMembersComponent extends PagedListingComponentBas
         private _organizationUnitService: OrganizationUnitServiceProxy) {
         super(_injector);
     }
-    
+
+
+    get organizationUnit(): IBasicOrganizationUnitInfo {
+        return this._organizationUnit;
+    }
+
+    set organizationUnit(ou: IBasicOrganizationUnitInfo) {
+        if (this._organizationUnit === ou) {
+            return;
+        }
+
+        this._organizationUnit = ou;
+        if (ou) {
+            this.refresh();
+        }
+    }
+
     protected list(request: PagedRequestDto, pageNumber: number, finishedCallback: Function): void {
+        if (this._organizationUnit != null) {
+            this._organizationUnitService
+                .getOrganizationUnitUsers(this._organizationUnit.id, undefined, request.maxResultCount, request.skipCount)
+                .finally(() => { })
+                .subscribe((result) => {
+                    this.dataItems = result.items;
+                });
+        }
         // this.loading=true;
         // this._organizationUnitService.getOrganizationUnitUsers(undefined,undefined,request.maxResultCount,request.skipCount)
-		// 	.finally(() => {
-		// 		finishedCallback();
-		// 		this.loading = false;
-		// 	})
-		// 	.subscribe((result) => {
-		// 		this.dataItems = result.items;
-		// 		//this.showPaging(result, pageNumber);
-		// 	});
+        // 	.finally(() => {
+        // 		finishedCallback();
+        // 		this.loading = false;
+        // 	})
+        // 	.subscribe((result) => {
+        // 		this.dataItems = result.items;
+        // 		//this.showPaging(result, pageNumber);
+        // 	});
         // this.permission.isGranted("");
-		//this.appSession.user
+        //this.appSession.user
     }
     protected delete(entity: OrganizationUnitUserListDto): void {
         // throw new Error("Method not implemented.");
     }
 
 
-    
+
 
     onEvent(ev: any) {
 
